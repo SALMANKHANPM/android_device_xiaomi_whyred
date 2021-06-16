97efa3a2a50998ebb3c43dcf834b8a35a9cd9178
SALMANKHANPM <patanmohammadsalmankhan@gmail.com>
2021-06-16 23:34:23 +0530

    Whyred : Spark~ifY !!
    - Added System.prop
    - Uprev To Redfin July FP

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_whyred.mk
+    $(LOCAL_DIR)/spark_whyred.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_whyred-user \
-    lineage_whyred-userdebug \
-    lineage_whyred-eng
+    spark_whyred-user \
+    spark_whyred-userdebug \
+    spark_whyred-eng
--- spark_whyred.mk
+++ spark_whyred.mk
@@ -14,24 +14,34 @@
 # limitations under the License.
 #
 
-# Inherit from those products. Most specific first.
+# Inherit from those products. Most specific first !
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
-# Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common #SparkOS stuff !
+$(call inherit-product, vendor/spark/config/common_full_phone.mk)
 
-# Inherit from whyred device
+# Inherit from whyred device !
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
-# Inherit from custom vendor.
+# Inherit from custom vendor !
 $(call inherit-product, vendor/MiuiCamera/config.mk)
 
+# SparkOS Properties !
+TARGET_INCLUDE_WIFI_EXT := true
+TARGET_GAPPS_ARCH := arm64
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+SPARK_BUILD_TYPE := WALKERZ-EDITION
+
+# Device Specfic !
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := whyred
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := lineage_whyred
+PRODUCT_NAME := spark_whyred
 PRODUCT_MODEL := Redmi Note 5
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
@@ -39,6 +49,6 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 TARGET_VENDOR_PRODUCT_NAME := whyred
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="redfin-user 11 RQ2A.210505.003 7255357 release-keys"
+        PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.210705.001 7380771 release-keys"
 
-BUILD_FINGERPRINT := google/redfin/redfin:11/RQ2A.210505.003/7255357:user/release-keys
+BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.210705.001/7380771:user/release-keys
